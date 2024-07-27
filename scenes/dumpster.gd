caa92c84932ffa6972de232a89d4fb65818d0c36
3120weissee <jackweisse@gmail.com>
2024-07-27 15:47:23 -0500

    Adding Dumpster
    Collision not working, but dumpster sprite is there.

---
+++ scenes/dumpster.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# create_inventory()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+	
+	
+
+func _on_area_entered(_area):
+	print('HELLO NURSE!')

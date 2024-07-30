9176c57e099de37cf3b1c91ed0b4df072696168b
Jon Weisse <jonathonweisse@gmail.com>
2024-07-30 14:16:10 -0500

    Created UFO Mob

--- scripts/city_collection.gd
+++ scripts/city_collection.gd
@@ -1,11 +1,42 @@
 extends Node2D
 
+@export var mob_scene: PackedScene
+var mobCounter = 0
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	%MobTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_mob_timer_timeout():
+		var mob = mob_scene.instantiate()
+		
+		var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+		mob_spawn_location.progress_ratio = randf()
+		
+		var direction = mob_spawn_location.rotation + PI / 2
+		
+		mob.position = mob_spawn_location.position
+		mob.randomizeScale()
+		
+		direction += randf_range(-PI / 4, PI / 4)
+		
+		var velocity = Vector2(randf_range(100.0,150.0), 0.0)
+		mob.linear_velocity = velocity.rotated(direction)
+		
+		add_child(mob)
+		mobCounter += 1
+		
+		updateMobTimer(mobCounter)
+
+func updateMobTimer(count):
+	var waitTime = %MobTimer.wait_time
+	if waitTime > .20:
+		if count % 10 == 0:
+			%MobTimer.wait_time -= .02
---
+++ scripts/ufo.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+	
+func randomizeScale():
+	var size = randf_range(0.75,2.0)
+	%UFOSprite.scale = Vector2(2.5-size,2.5-size)
+	%UFOLight.scale = Vector2(size,size)
+	%Detection.scale = Vector2(size,size)
